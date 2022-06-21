b206c5bbe143e09bf4f31be43eb6b78ea0cebac8
Sary-Sary <56128679+Sary-Sary@users.noreply.github.com>
2022-06-21 09:36:36 +0300

    Add files via upload

--- PGM_File.cpp
+++ PGM_File.cpp
@@ -22,9 +22,50 @@ void PGM_File::negative() {
 
 }
 
+std::vector<Greyscale> PGM_File::rotate_left() {
+
+	std::vector<Greyscale> rotated_matrix = file;
+
+	for (int i = 0; i < width; i++) {
+
+		for (int j = 0; j < height; j++) {
+
+			rotated_matrix[j * width - i - 1 + height] = file[i*width + j];
+
+		}
+
+	}
+
+	return rotated_matrix;
+
+}
+
+void rotate_right() {
+
+
+
+
+}
+
 void PGM_File::rotate(std::string direction) {
 
+	if (direction == "left") {
 
+		file = rotate_left();
+		std::swap(height, width);
+		return;
+	}
+
+	if (direction == "right") {
+
+		file = rotate_right();
+		std::swap(height, width);
+		return;
+
+	}
+
+	std::cout << "Invalid direction\n";
+	return;
 
 }
 
